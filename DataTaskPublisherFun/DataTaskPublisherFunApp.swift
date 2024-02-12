371452fde0449fab90ad39d92a0b2c7b0b979cdb
jwigel <nigeljwright22@gmail.com>
2024-02-12 12:19:01 -0600

    Initial Commit

---
+++ DataTaskPublisherFun/DataTaskPublisherFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataTaskPublisherFunApp.swift
+//  DataTaskPublisherFun
+//
+//  Created by Nigel Wright on 12/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DataTaskPublisherFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
